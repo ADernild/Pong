510b2aec793585d417f18d5719c2377454e534a0
ADernild <46223274+ADernild@users.noreply.github.com>
2018-12-28 15:16:18 +0100

    Add files via upload

---
+++ player.js
@@ -0,0 +1,37 @@
+function Player() {
+    this.w = 15;
+    this.h = 80;
+    this.pos = createVector(this.w*2, height/2-this.h/2);
+    this.acc = createVector(0, 0);
+    this.spd = 5;
+    this.maxSpd = 10;
+    
+//Tegner spillerens firkant figur
+this.show = function() {
+        noStroke();
+        fill(255);
+        rect(this.pos.x, this.pos.y, this.w, this.h)
+    }
+this.update = function() {
+        this.acc.y = constrain(this.acc.y, -this.maxSpd, this.maxSpd);
+        this.pos.add(this.acc);
+        this.pos.y = constrain(this.pos.y, 0, height-this.h);
+    }
+
+//Funktionen der bruges når man trykker pil op
+this.up = function() {
+        this.acc.y-=this.spd;
+                        }
+
+//Funktionen der bruges når man trykker pil ned
+this.down = function() {
+        this.acc.y=this.spd;
+    }
+
+//Funktionen der bruges til at stoppe når man ikke trykker på pilene
+this.stp = function() {
+        this.acc.y = 0;
+    }
+    
+
+}
---
+++ sketch.js
@@ -0,0 +1,79 @@
+var ball, player, ai ;
+
+var playerScore = 0;
+var AIScore = 0;
+
+var dots = [];
+var dSize = 10;
+var txtSize = 30;
+
+function setup() {
+    createCanvas(800, 500);
+    
+    ball = new Ball();
+    player = new Player();
+    ai = new AI();
+    
+    for(let y = dSize/2; y<height; y+=dSize*2){
+        dots.push(createVector(width/2-dSize/2, y));
+        
+    }
+}
+//Tegne funktionen
+function draw() {
+    background(0);
+    
+    ball.update();
+    ball.show();
+    ai.show();
+    ai.update();
+    player.show();
+    player.update();
+    ball.edges();
+    ball.scores();
+    
+    drawScores();
+    
+    
+    noStroke();
+    fill(255);
+    drawSquares();
+}
+
+function drawScores(){
+    let x1 = width/4;
+    let x2 = width*3/4;
+    let y = txtSize*1.5;
+    
+    noStroke();
+    fill(255);
+    textAlign(CENTER);
+    textSize(txtSize);
+    text(playerScore, x1, y);
+    text(AIScore, x2, y);
+    
+}
+
+function drawSquares() {
+    for(let i = 0; i<dots.length; i++){
+        let x = dots[i].x;
+        let y = dots[i].y;
+        
+        rect(x, y, dSize, dSize);
+    }
+    
+}
+
+function keyPressed(){
+    if(key == 'w' || keyCode==UP_ARROW){
+        player.up();
+    } else if(key == 'S' || keyCode==DOWN_ARROW){
+        player.down();
+    }
+}
+
+function keyReleased(){
+    if((key == 'W' || keyCode==UP_ARROW) ||(key=='S' || keyCode==DOWN_ARROW)){
+        player.stp();
+    }
+}
